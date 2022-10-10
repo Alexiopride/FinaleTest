863191b52402d112a9374864df09328d847ed5d2
alexiopride <alexiopride@gmail.com>
2022-10-10 18:50:25 +0400

    Added task solution

--- Program.cs
+++ Program.cs
@@ -5,3 +5,36 @@
 Задача решена 2мя способами: с помощью списков (закомментировано) и обычным.
 Алгоритм составлен ко второму способу.
 */
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[num];
+int arrayLength = 0;
+
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3)
+        arrayLength++;
+}
+
+if (arrayLength > 0)
+{
+    string[] arrayNew = new string[arrayLength];
+    Console.WriteLine("Массив из элементов, длина которых <=3 символов: ");
+    int indexNew = 0;
+
+    for (int i = 0; i < num; i++)
+    {
+         if (array[i].Length <= 3)
+        {
+            arrayNew[indexNew]=array[i];
+            Console.WriteLine($"Это {indexNew+1} элемент массива: {arrayNew[indexNew]}");
+            indexNew++;
+        }
+    }
+}
+else
+    Console.WriteLine("Не было ни одного элемента меньше 3х символов длиной.");
